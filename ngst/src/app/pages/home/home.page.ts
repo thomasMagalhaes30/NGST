65126368a16f6226d3c1169a1e0814375f768e83
Amiralgaby <gaby.theuws@gmail.com>
2022-06-03 18:50:17 +0200

    image du jour + bouton voir HD

--- ngst/src/app/pages/home/home.page.ts
+++ ngst/src/app/pages/home/home.page.ts
@@ -9,16 +9,32 @@ export class HomePage {
   message = "edde";
 
   apod = {
-    "date" : "2001-12-29",
-    "explanation" : "The sky toward the center of our Galaxy is filled with a wide variety of celestial wonders. Many are easily visible with binoculars.   Constellations near the galactic center include Sagittarius, Libra, Scorpius, Scutum, and Ophiuchus.  Nebulae include Messier Objects M8, M16, M17, M20 and the Pipe Nebula.  Open star clusters include M6, M7, M18, M21, M23, M24, M25.   Globular star clusters include M9, M22, M28, M54, M69, M70.  And don't forget Baade's Window.  Click on the photo to get the un-annotated version.",
-    "hdurl" : "https://apod.nasa.gov/apod/image/0112/galcen_wk_big.gif",
-    "title" : "The Annotated Galactic Center",
-    "url" : "https://apod.nasa.gov/apod/image/0112/galcen_wk.gif"
+    "date" : "",
+    "explanation" : "",
+    "hdurl" : "",
+    "title" : "",
+    "url" : ""
   };
 
   // todo ajouter le service par injection de dépendances dans le constructeur
   constructor() {
-    console.log(this.apod);
+    let today = new Date();
+    let todayString = today.getFullYear() + "-" + today.getMonth() + "-" + today.getDay();
+
+    console.log(todayString);
+
+    fetch("https://api.nasa.gov/planetary/apod?date="+ todayString + "&api_key=BlNiKpyUAovsd7JgTbofzaqUkFrYoFwpAI63SE8x")
+    .then(response => response.json())
+    .then(data => {
+      if (data === undefined || data === null)
+        return;
+      this.apod.date = data.date;
+      this.apod.url = data.url;
+      this.apod.title = data.title;
+      this.apod.hdurl = data.hdurl;
+      this.apod.explanation = data.explanation;
+    })
+    .catch(console.error);
   }
 
 }
