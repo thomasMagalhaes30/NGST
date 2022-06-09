a70abab005c0d8e0e62c00426fb64dbddbb1c0d6
GODZI <thomas.magalhaes00@gmail.com>
2022-06-09 12:58:57 +0200

    fix import pour passer les tests

--- ngst/src/app/pages/home/home.page.spec.ts
+++ ngst/src/app/pages/home/home.page.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { HomePage } from './home.page';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('HomePage', () => {
   let component: HomePage;
@@ -10,7 +11,10 @@ describe('HomePage', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
-      imports: [IonicModule.forRoot()]
+      imports: [
+        IonicModule.forRoot(),
+        HttpClientTestingModule
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
