988f2c54a94a5a60ffa238108aeaf58fb0eca7fc
Amiralgaby <gaby.theuws@gmail.com>
2022-06-15 19:01:19 +0200

    [FIX] test apod

--- ngst/src/app/components/apod/apod.component.spec.ts
+++ ngst/src/app/components/apod/apod.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ApodComponent } from './apod.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('ApodComponent', () => {
   let component: ApodComponent;
@@ -10,7 +11,7 @@ describe('ApodComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ApodComponent ],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot() , HttpClientTestingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ApodComponent);
