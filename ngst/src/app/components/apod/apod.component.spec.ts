53c3a3486392a6ed82070bebf1a77ec7769f9696
Amiralgaby <gaby.theuws@gmail.com>
2022-06-26 12:21:52 +0200

    add test on apod component

--- ngst/src/app/components/apod/apod.component.spec.ts
+++ ngst/src/app/components/apod/apod.component.spec.ts
@@ -6,6 +6,7 @@ import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('ApodComponent', () => {
   let component: ApodComponent;
+  let debugElement;
   let fixture: ComponentFixture<ApodComponent>;
 
   beforeEach(waitForAsync(() => {
@@ -16,10 +17,36 @@ describe('ApodComponent', () => {
 
     fixture = TestBed.createComponent(ApodComponent);
     component = fixture.componentInstance;
+    debugElement = fixture.debugElement;
+    component.apod = {
+      "resource": "string",
+      "concept_tags": "string",
+      "title": "Light Echoes from V838 Mon",
+      "date": "22-01-2022",
+      "url": "https://apod.nasa.gov/apod/image/2206/V838Mon_Hubble_960.jpg",
+      "hdurl": "https://apod.nasa.gov/apod/image/2206/V838Mon_Hubble_2238.jpg",
+      "media_type" : "image",
+      "explanation" : "What caused this outburst of V838 Mon? For reasons unknown...",
+      "concepts" : "string",
+      "thumbnail_url" : "string",
+      "copyright" : "string",
+      "service_version" : "string",
+    };
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show apod info', () => {
+    expect(debugElement.nativeElement.querySelector('ion-card-title').textContent)
+        .toEqual('Light Echoes from V838 Mon');
+
+    expect(debugElement.nativeElement.querySelector('#apodImg').src)
+         .toEqual('https://apod.nasa.gov/apod/image/2206/V838Mon_Hubble_960.jpg');
+
+    expect(debugElement.nativeElement.querySelector('p').textContent)
+      .toEqual('What caused this outburst of V838 Mon? For reasons unknown...');
   });
 });
